057a4dfd92f4ce3eda13ba4d045f5ec0f2d71c88
Clavvv <Schpeen@proton.me>
2024-04-02 10:47:39 -0700

    wifiqfhqwef

---
+++ app/Video.js
@@ -0,0 +1,82 @@
+
+
+
+export class Video {
+
+    constructor(url) {
+
+        this.videoElement= document.createElement('video')
+        this.canvas = document.createElement('canvas')
+        this.context = this.canvas.getContext('2d')
+
+        this.width= 640
+        this.height= 480
+
+        this.videoElement.width= this.width
+        this.videoElement.height= this.height
+        this.videoElement.muted= false
+        this.videoElement.loop= true
+        this.videoElement.src= url
+
+        this.stream= null
+
+        this.frameListeners= []
+
+    }
+
+    addFrameListener = (listener) => {
+
+        this.frameListeners.push(listener)
+
+    }
+
+    removeFrameListener = (listener) => {
+        
+        this.frameListeners= this.frameListeners.filter(l => l !== listener)
+
+    }
+
+    start() {
+
+        this.videoElement.play()
+        this.videoElement.addEventListener('play', () => this.processVideo())
+
+    }
+
+    stop() {
+
+        this.videoElement.pause()
+        this.videoElement.srcObject= null
+
+        while (this.videoElement.firstChild) {
+            this.videoElement.removeChild(this.videoElement.firstChild)
+        }
+
+        if (this.stream){
+
+            this.stream.getTracks().forEach(track => track.stop())
+
+
+        }
+
+        this.frameListeners= []
+    }
+
+    processVideo = async () => {
+
+        const canvas= new OffscreenCanvas(this.videoElement.width, this.videoElement.height)
+        const context= canvas.getContext('2d')
+
+        const frame = () => {
+
+            context.drawImage(this.videoElement, 0, 0, this.width, this.height)  
+            const imageData= context.getImageData(0, 0, this.width, this.height)
+            
+            this.frameListeners.forEach(listener => listener(imageData))
+            requestAnimationFrame(frame)
+
+        }
+        requestAnimationFrame(frame)
+    }
+
+}
--- app/page.js
+++ app/page.js
@@ -1,113 +1,228 @@
-import Image from "next/image";
+'use client';
+import React, { useState, useRef, useEffect } from 'react';
+import { Video } from "./Video.js";
 
 export default function Home() {
+
+  const [selected, setSelected] = useState(1)
+  const [fileError, setFileError]= useState(false)
+  const [asciiArt, setAsciiArt]= useState(null)
+
+  const videoProcessorRef= useRef(null)
+
+  const isSelected= "border bg-zinc-600 rounded-md mx-2 p-1 border-purple-700 justify-center place-self-center text-white"
+  const notSelected= "border border-zinc-900 rounded-md mx-2 p-1 justify-center place-self-center "
+
+  const brightnessToAscii = (levels) => {
+
+    const asciiChars= '@%#*+=-:. '.split('').reverse()
+
+    const sum= (levels[0] * 0.299) + (levels[1] * 0.587) + (levels[2] * 0.114) //floats are perceived brightness for each color
+    const brightness= sum / 255                                                 //normalizing the brightness to 0 and 1
+
+    return asciiChars[Math.floor(brightness * (asciiChars.length - 1))]         //selecting the ascii char based on brightness
+
+  }
+  const convertToAscii = (imageData) => {
+
+    let asciiImage= ''
+
+    const { data, width, height }= imageData
+
+    for (let i= 0; i < height; i += 4){                                       //We skip every other row because ascii chars too tall
+
+      for (let j= 0; j < width; j += 2) {
+
+        const offset= (i * width + j) * 4                                     //image array is 1-dimensional. So i = row, j = column, * 4 bc there are 4 values per pixel
+        const r= data[offset]
+        const g= data[offset + 1]
+        const b= data[offset + 2]
+        const values= [r, g, b]
+
+        asciiImage+= brightnessToAscii(values)
+
+    }
+    asciiImage+= '\n'
+
+  }
+  return asciiImage
+}
+
+  
+  const handleSelected = (e) => {
+
+    e.preventDefault()
+
+    if (e.target.id === "Upload") {
+      setSelected(1)
+    } else {
+      setSelected(2)
+    }
+
+  }
+
+  const handleImageUpload = (e) => {
+
+
+    e.preventDefault()
+
+    const file= e.target.files[0]
+    const reader= new FileReader()
+
+    if (!file) {
+
+      setFileError(true)
+      return
+
+    } else if ((file.type !== 'image/jpeg' && file.type !== 'image/png') || file.size > 100000000 ) {
+
+      setFileError(true)
+      return 
+
+    } else {
+
+      setAsciiArt(null)
+      setFileError(false);
+        reader.onload = async (e) => {
+            const image = new Image()
+            image.onload = async () => {
+                const canvas = document.createElement('canvas')
+                const ctx = canvas.getContext('2d')
+
+
+                canvas.width = image.width
+                canvas.height = image.height
+
+                ctx.drawImage(image, 0, 0)
+
+                const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height)
+                const asciiImage = convertToAscii(imageData)
+
+                setAsciiArt(asciiImage)
+            }
+            image.src = e.target.result
+        };
+        reader.readAsDataURL(file)
+
+    }
+  }
+
+  const handleVideoUpload = async (e) => {
+
+    e.preventDefault()
+
+    const file= e.target.files[0]
+
+    if (!file) {
+
+      setFileError(true)
+      return
+    } else if (file.type !== 'video/mp4') {
+
+      setFileError(true)
+      return
+
+    }
+
+    const reader= new FileReader()
+    reader.readAsDataURL(file)
+
+    reader.onload = (e) => {
+
+      console.log(e.target.result)
+      const videoPlayer= new Video(e.target.result)
+
+      videoPlayer.addFrameListener((frame) => {
+        
+        const asciiString= convertToAscii(frame)
+        setAsciiArt(asciiString)
+      })
+
+
+    videoPlayer.start()
+
+    }
+
+  }
+  
+
+
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="flex grow min-h-screen min-w-screen flex-col items-center pt-14 px-14 bg-zinc-900 overflow-hidden">
+
+      <p>
+        <span className= "text-4xl tracking-widest font-bold italic font-system-ui bg-gradient-to-r from-purple-400 to-pink-500 text-transparent bg-clip-text">
+          A
+        </span>
+        <span className= "text-2xl font-semibold tracking-widest font-system-ui bg-gradient-to-r from-purple-400 to-pink-500 text-transparent bg-clip-text">
+          scii
+        </span>
+      </p>
+
+      <div className= "flex-col grow items-center justify-center p-10">
+
+        <div id= "types" className= 'flex flex-row my-5 justify-center'>
+
+          {selected === 1 ? (<button id= "Upload" onClick= {(e) => handleSelected(e)} className= {isSelected}>Upload</button>) : (
+          <button id= "Upload" onClick= {(e)=> handleSelected(e)} className= {notSelected}>Upload</button>)}
+
+          {selected === 2 ? (<button id= "Webcam" onClick= {(e)=> handleSelected(e)} className= {isSelected}>Webcam</button>) : (<button id= "Webcam" onClick= {(e)=> handleSelected(e)} className= {notSelected}>Webcam</button>)}
+
+
         </div>
+        {selected === 1 ? (
+        <section className= "flex-row w-full h-full">
+          
+          
+          <input className= "hidden" type= "file" id= "image-input" accept="image/*" onChange= {handleImageUpload}>
+          </input>
+
+          <input className= "hidden" type= "file" id= "video-input" accept="video/*" onChange= {handleVideoUpload}>
+          </input>
+
+          <ul className= "flex flex-row justify-center">
+
+            <li className= "rounded-md m-2 px-3 py-1 justify-center text-center hover:bg-zinc-500">
+
+              <button className= "justify-center" onClick= {() => document.getElementById('image-input').click()}>
+                Image
+              </button>
+
+            </li>
+
+            <li className= "rounded-md m-2 px-3 py-1 justify-center text-center hover:bg-zinc-500">
+
+              <button className= "justify-center" onClick= {() => document.getElementById('video-input').click()}>
+                Video
+              </button>
+
+            </li>
+
+          </ul>
+
+          {fileError && (<p className= "text-center text-red-500 text-sm italic font-semibold">invalid file type or size</p>)}
+
+        </section>) : (
+          <section className= "flex-row w-full h-full justify-evenly">
+
+          <ul className= "flex flex-row justify-center place-items-center content-center items-center place-content-center">
+
+            <li className= "rounded-md m-2 px-3 py-1 justify-center text-center hover:bg-zinc-500">
+
+              <button>
+                Start
+              </button>
+
+            </li>
+
+          </ul>
+
+        </section>)}
+        {asciiArt && (<pre className= "text-white flex max-w-[40vw] max-h-[70vh] text-[3px] pt-5 m-5 justify-center overflow-auto">{asciiArt}</pre>)}
+
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
     </main>
   );
 }
